72e64e45193782ba8c6d63eb3e05e456d08f07ae
akmalm007 <96603135+akmalm007@users.noreply.github.com>
2023-06-04 20:00:34 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "34.101.215.102",
-  USER: "root",
-  PASSWORD: "t0em=M0pV{tg$u:8",
-  DB: "prodify_db",
+  HOST: "isi_hostname",
+  USER: "isi_user",
+  PASSWORD: "isi_password",
+  DB: "isi_db",
   dialect: "mysql",
   pool: {
     max: 5,
